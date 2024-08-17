15d48ac5f553acdc8d3e7b05e0c3603d5b04e389
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-08-17 18:10:41 +0530

    complete basic refactor

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import altair as alt
 from scraper.result_scraper import get_results
 from scraper.metadata_scraper import get_exam_metadata
+from utils.data_formatting import extract_sem_results_categorized_on_program
+from utils.data_formatting import extract_courses_details_categorized_on_program
+from utils.data_formatting import extract_bar_chart_data
 
 SEMESTERS = [
     "FIRST SEMESTER",
@@ -31,7 +34,7 @@ def main():
 
     start_prn, end_prn = select_prn_range()
 
-    if not st.button("Run Scraping"):
+    if not st.button("Run Scraping", type="primary"):
         return
 
     if not all([selected_exam_id, start_prn, end_prn]):
@@ -108,14 +111,18 @@ def display_scraped_data(scraped_data, selected_semester):
 
     display_scraped_data_header_info(selected_semester)
 
-    # Refactor from here
-    extracted_data, coursecode_to_coursename = extract_major_fields(scraped_data)
+    sem_results_categorized_on_program = extract_sem_results_categorized_on_program(
+        scraped_data
+    )
+    course_details_categorized_on_program = (
+        extract_courses_details_categorized_on_program(scraped_data)
+    )
 
-    for program in extracted_data:
-        display_results_table_and_charts(
+    for program in sem_results_categorized_on_program:
+        display_scraped_data_of_one_program(
             program,
-            extracted_data[program],
-            coursecode_to_coursename[program],
+            sem_results_categorized_on_program[program],
+            course_details_categorized_on_program[program],
         )
 
 
@@ -128,119 +135,20 @@ def display_scraped_data_header_info(selected_semester):
     st.info("Hover over the table's to find the download button for CSV file.")
 
 
-def display_results_table_and_charts(program_name, student_data, coursecode_to_name):
+def display_scraped_data_of_one_program(program, sem_studs_result, sem_courses_data):
     st.markdown(
-        f"###### <u>{program_name} ({len(student_data)} Entries)</u>",
+        f"###### <u>{program} ({len(sem_studs_result)} nos)</u>",
         unsafe_allow_html=True,
     )
-    st.dataframe(student_data)
+    st.dataframe(sem_studs_result)
 
     st.markdown(f"**Course Details**")
-    st.dataframe(coursecode_to_name)
+    st.dataframe(sem_courses_data)
 
-    bar_chart_data = get_bar_chart_data(student_data)
+    bar_chart_data = extract_bar_chart_data(sem_studs_result)
     st.altair_chart(
         get_grade_distribution_chart_data(bar_chart_data), use_container_width=True
     )
-
-
-def extract_major_fields(data):
-    program_wise_flattened_result = {}
-    coursecode_to_coursename_map = {}
-
-    for d in data:
-        if d:
-            program = d["personal_details"]["program"]
-
-            if program not in coursecode_to_coursename_map:
-                coursecode_to_coursename_map[program] = []
-                for x in d["subjects_results"]:
-                    course_code = x["course_code"]
-                    course_name = x["course"]
-                    credit = x["credit"]
-                    max_external = x["max_ext"]
-                    max_internal = x["max_int"]
-                    max = x["max"]
-                    coursecode_to_coursename_map[program].append(
-                        {
-                            "Course Code": course_code,
-                            "Course Name": course_name,
-                            "Credit": credit,
-                            "Max Internal": max_internal,
-                            "Max External": max_external,
-                            "Max Marks": max,
-                        }
-                    )
-
-            temp = {}
-            temp["PRN"] = d["personal_details"]["prn"]
-            temp["Name"] = d["personal_details"]["name"]
-            temp["Exam centre"] = d["personal_details"]["exam_centre"]
-
-            for x in d["subjects_results"]:
-                course = x["course_code"]
-                temp[f"{course} ISA"] = x["isa_int"]
-                temp[f"{course} ESA"] = x["esa_ext"]
-                temp[f"{course} Total"] = x["total"]
-                temp[f"{course} Grade"] = x["grade"]
-
-            temp["SCPA"] = d["overall_sem_result"]["scpa"]
-            temp["Total Mark"] = d["overall_sem_result"]["total_marks"]
-            temp["Grade"] = d["overall_sem_result"]["grade"]
-
-            if program not in program_wise_flattened_result:
-                program_wise_flattened_result[program] = []
-
-            program_wise_flattened_result[program].append(temp)
-
-    for x in coursecode_to_coursename_map:
-        program_row = coursecode_to_coursename_map[x]
-
-        sum_credit = 0
-        sum_max_internal = 0
-        sum_max_external = 0
-        sum_max_marks = 0
-
-        for i in program_row:
-            sum_credit += int(i["Credit"])
-            sum_max_internal += int(i["Max Internal"])
-            sum_max_external += int(i["Max External"])
-            sum_max_marks += int(i["Max Marks"])
-
-        program_row.append(
-            {
-                "Course Code": "Total",
-                "Course Name": "",
-                "Credit": str(sum_credit),
-                "Max Internal": str(sum_max_internal),
-                "Max External": str(sum_max_external),
-                "Max Marks": str(sum_max_marks),
-            }
-        )
-
-    return (program_wise_flattened_result, coursecode_to_coursename_map)
-
-
-def get_bar_chart_data(data):
-    grades_order = ["S", "A+", "A", "B+", "B", "C", "D", "Fail"]
-    grade_count = {}
-
-    for d in data:
-        student_grade = d["Grade"] if d["Grade"] != "---" else "Fail"
-
-        if student_grade in grade_count:
-            grade_count[student_grade] += 1
-        else:
-            grade_count[student_grade] = 1
-
-    # Convert the dictionary to a list of tuples and sort it in place
-    grade_count_items = list(grade_count.items())
-    grade_count_items.sort(key=lambda x: grades_order.index(x[0]))
-
-    # Convert the sorted list of tuples back to a dictionary
-    sorted_grade_count = dict(grade_count_items)
-
-    return sorted_grade_count
 
 
 def get_grade_distribution_chart_data(bar_chart_data):
--- src/utils/data_formatting.py
+++ src/utils/data_formatting.py
@@ -1,1 +1,116 @@
+def extract_sem_results_categorized_on_program(scraped_data):
+    """Extract students sem results categorized on program"""
+    results_categorized_on_program = {}
+    for stud_data in scraped_data:
+        if not stud_data:
+            continue
 
+        program = stud_data["personal_details"]["program"]
+        if program not in results_categorized_on_program:
+            results_categorized_on_program[program] = []
+
+        major_fields = extract_sem_results_major_fields(stud_data)
+        results_categorized_on_program[program].append(major_fields)
+
+    return results_categorized_on_program
+
+
+def extract_sem_results_major_fields(stud_data):
+    """Extract major fields of a students sem results"""
+    major_fields = {}
+    major_fields["PRN"] = stud_data["personal_details"]["prn"]
+    major_fields["Name"] = stud_data["personal_details"]["name"]
+    major_fields["Exam centre"] = stud_data["personal_details"]["exam_centre"]
+
+    for sub_data in stud_data["subjects_results"]:
+        course = sub_data["course_code"]
+        major_fields[f"{course} ISA"] = sub_data["isa_int"]
+        major_fields[f"{course} ESA"] = sub_data["esa_ext"]
+        major_fields[f"{course} Total"] = sub_data["total"]
+        major_fields[f"{course} Grade"] = sub_data["grade"]
+
+    major_fields["SCPA"] = stud_data["overall_sem_result"]["scpa"]
+    major_fields["Total Mark"] = stud_data["overall_sem_result"]["total_marks"]
+    major_fields["Grade"] = stud_data["overall_sem_result"]["grade"]
+
+    return major_fields
+
+
+def extract_courses_details_categorized_on_program(scraped_data):
+    """Categorize course details based on the program"""
+    course_details_categorized_on_program = {}
+    for stud_data in scraped_data:
+        if not stud_data:
+            continue
+
+        program = stud_data["personal_details"]["program"]
+
+        if program in course_details_categorized_on_program:
+            continue
+
+        course_details = extract_course_details(stud_data)
+        course_details_categorized_on_program[program] = course_details
+
+    return course_details_categorized_on_program
+
+
+def extract_course_details(stud_data):
+    """Extract course details for a given student's data"""
+    course_details = []
+    for sub_data in stud_data["subjects_results"]:
+        course_code = sub_data["course_code"]
+        course_name = sub_data["course"]
+        credit = sub_data["credit"]
+        max_external = sub_data["max_ext"]
+        max_internal = sub_data["max_int"]
+        max_marks = sub_data["max"]
+
+        course_details.append(
+            {
+                "Course Code": course_code,
+                "Course Name": course_name,
+                "Credit": credit,
+                "Max Internal": max_internal,
+                "Max External": max_external,
+                "Max Marks": max_marks,
+            }
+        )
+    course_details.append(calculate_courses_totals(course_details))
+
+    return course_details
+
+
+def calculate_courses_totals(course_details):
+    """Calculate total credit, internal, external numerics of all courses combined in a sem"""
+    total_credit = 0
+    total_max_internal = 0
+    total_max_external = 0
+    total_max_marks = 0
+    for course in course_details:
+        total_credit += int(course["Credit"])
+        total_max_internal += int(course["Max Internal"])
+        total_max_external += int(course["Max External"])
+        total_max_marks += int(course["Max Marks"])
+
+    return {
+        "Course Code": "Total",
+        "Course Name": f"{len(course_details)} nos",
+        "Credit": str(total_credit),
+        "Max Internal": str(total_max_internal),
+        "Max External": str(total_max_external),
+        "Max Marks": str(total_max_marks),
+    }
+
+
+def extract_bar_chart_data(data):
+    """Get bar chart data on Grade(x) to Students(y)"""
+    grades_order = ["S", "A+", "A", "B+", "B", "C", "D", "Fail"]
+    grade_count = {grade: 0 for grade in grades_order}
+
+    for d in data:
+        student_grade = d["Grade"] if d["Grade"] != "---" else "Fail"
+        grade_count[student_grade] += 1
+
+    return {
+        grade: grade_count[grade] for grade in grades_order if grade_count[grade] > 0
+    }
